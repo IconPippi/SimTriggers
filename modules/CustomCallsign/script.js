93aefda0916eebbf35729d80e5f1845ab46262dd
IconPippi <casadio.filo@gmail.com>
2019-06-20 13:02:32 +0200

    Final update

--- modules/CustomCallsign/script.js
+++
@@ -1,7 +0,0 @@
-var RegisterTrigger = Java.type("com.IconPippi.simtriggers.triggers.RegisterTrigger");
-
-RegisterTrigger.registerKey("testKey").setKey("Shift+U");
-
-function testKey() {
-    print("works");
-}
