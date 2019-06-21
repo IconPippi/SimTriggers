0fa54a4482dcd9121efd1b2c0d0c9627e85a4754
IconPippi <casadio.filo@gmail.com>
2019-06-21 18:05:39 +0200

    Fix

---
+++ src/main/java/com/IconPippi/simtriggers/Main.java
@@ -0,0 +1,35 @@
+package com.IconPippi.simtriggers;
+
+import com.IconPippi.simtriggers.utils.Logger;
+
+import flightsim.simconnect.SimConnect;
+
+/**
+ * Hello there
+ * @author IconPippi
+ */
+public class Main {
+	
+	/*
+	 * SimConnect constant
+	 */
+	public static SimConnect simConnect;
+	
+	public static void main(String[] args) {
+		final Logger logger = new Logger();
+		logger.log("Initializing SimTriggers");
+		
+		final ConnectionOpen co; //Connection class
+		
+		//Establish connection
+		try {
+			co = new ConnectionOpen();
+			simConnect = co.getSimConnect();
+		} catch (Exception e) {
+			logger.error("Connection could not be initalized: \n"+e.toString());
+			return;
+		}
+		
+		co.run(); //Run connection thread
+	} 
+}
